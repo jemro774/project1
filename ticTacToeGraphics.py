17e77aaec0a9622dcf0362a2dbe83af35e5811ae
jemro774 <31485871+jemro774@users.noreply.github.com>
2017-11-01 14:01:06 -0400

    Updated from Brython Server: 11/1/2017 2:01:05 PM

--- ticTacToeGraphics.py
+++ ticTacToeGraphics.py
@@ -1,1 +1,22 @@
+#Jack Robey
+#11/1/17
+#ticTacToeGraphics.py - Tic-Tac-Toe with graphics, and possibly AI (with set difficulties)
+
+#imports
+from ggame import *
+from random import randint
+
+if __name__ == '__main__':
+    
+    black = Color(0x000000,1)
+    
+    blackOutline = LineStyle(5,black)
+    
+    blackLine1 = LineAsset(0,50,blackOutline)
+    
+    Sprite(blackLine1,(1012,0))
+    App().run()
+    
+
+
 
